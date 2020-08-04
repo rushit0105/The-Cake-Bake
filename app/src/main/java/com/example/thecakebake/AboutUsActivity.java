21b1bda1a46eb4b8a73792ea9351e16a90aa37db
KAJAL RAMANI <kramani409@rku.ac.in>
2020-08-04 17:23:00 +0530

    Rename activity_abou_us.xml as activity_about_us.xml

--- app/src/main/java/com/example/thecakebake/AboutUsActivity.java
+++ app/src/main/java/com/example/thecakebake/AboutUsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AbouUsActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
