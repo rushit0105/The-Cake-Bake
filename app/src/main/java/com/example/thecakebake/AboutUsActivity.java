f3d36e2ef66efa870daceb00413fd1d62e8867d0
rushit007 <rushit.l.kachadiya007@gmail.com>
2020-08-05 11:07:23 +0530

    All Activity title seted

--- app/src/main/java/com/example/thecakebake/AboutUsActivity.java
+++ app/src/main/java/com/example/thecakebake/AboutUsActivity.java
@@ -2,6 +2,7 @@ package com.example.thecakebake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 public class AboutUsActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        this.setTitle("About Us");
     }
 }
